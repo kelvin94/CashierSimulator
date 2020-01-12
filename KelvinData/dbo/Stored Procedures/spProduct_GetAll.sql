e8450b805d5b05f8c48285acaa593084b09a5e35
Kelvin Lin <kelvin94@live.cn>
2020-01-12 08:55:52 -0800

    Added new controller, new data access class and model

---
+++ KelvinData/dbo/Stored Procedures/spProduct_GetAll.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spProduct_GetAll]
+AS
+BEGIN
+SET NOCOUNT ON;
+
+	SELECT Id, ProductName, [Description], RetailPrice, QuantityInStock, IsTaxable
+	FROM [dbo].[Product]
+	ORDER BY ProductName;
+END
---
+++ KelvinDataManager.Library/DataAccess/ProductData.cs
@@ -0,0 +1,31 @@
+﻿using KelvinDataManager.Library.Internal.DataAccess;
+using KelvinDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KelvinDataManager.Library.DataAccess
+{
+    public class ProductData
+    {
+        public List<ProductModel> GetProducts()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetAll", new { }, "KelvinData");
+
+            return output;
+        }
+
+        public ProductModel GetProductById(int productId)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetById", new { Id = productId }, "KelvinData").FirstOrDefault();
+
+            return output;
+        }
+    }
+}
