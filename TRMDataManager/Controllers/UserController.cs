08ec15868b8e170ce27ac66437bd0dfe61758d4b
Kelvin Lin <kelvin94@live.cn>
2020-01-12 19:43:57 -0800

    Added end point to show all roles for each user.

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -1,18 +1,20 @@
 ﻿using KelvinDataManager.Library.DataAccess;
 using KelvinDataManager.Library.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using TRMDataManager.Models;
 
 namespace TRMDataManager.Controllers
 {
     [Authorize]
     public class UserController : ApiController
     {        
-
+        [HttpGet]
         // GET: User/Details/5
         public List<UserModel> GetById()
         {
@@ -22,6 +24,39 @@ namespace TRMDataManager.Controllers
             return data.GetUserById(userId);            
         }
 
+        [HttpGet]
+        [Route("api/User/Admin/GetAllUsers")]
+        public List<ApplicationUserModel> GetAllUsers()
+        {
+            List<ApplicationUserModel> output = new List<ApplicationUserModel>();
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var users = userManager.Users.ToList();
+
+                var roles = context.Roles.ToList();
+
+                foreach (var user in users)
+                {
+                    ApplicationUserModel u = new ApplicationUserModel
+                    {
+                        Id = user.Id,
+                        Email = user.Email
+                    };
+
+                    foreach (var role in user.Roles)
+                    {
+                        u.Roles.Add(role.RoleId, roles.Where(x => x.Id == role.RoleId).First().Name);
+                    }
+
+                    output.Add(u);
+                }
+                return output;
+            }
+            
+        }
         
     }
 }
